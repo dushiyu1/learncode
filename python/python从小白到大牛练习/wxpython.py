0b11e28b26a3868343df5159e74e8d38746e2b6e
dushiyu1 <dushiyu123@icloud.com>
2021-05-30 22:25:56 +0800

    2021.5.30 晚上

--- python/python从小白到大牛练习/wxpython.py
+++ python/python从小白到大牛练习/wxpython.py
@@ -4,8 +4,25 @@ class MyFrame(wx.Frame):
         super().__init__(parent=None,title="第一个gui程序",size=(400,300))
         self.Centre()
         panel = wx.Panel(parent=self)
-        statictext = wx.StaticText(parent=panel,label="hello,world",pos=(100,100))
+        self.statictext = wx.StaticText(parent=panel,pos=(110,20))
+        b1 = wx.Button(parent=panel,label="1",id = 11, pos=(100,50))
+        b2 = wx.Button(parent=panel,label="2",id = 12, pos=(100,100))
+        b2 = wx.Button(parent=panel,label="2",id = 13, pos=(100,150))
+
+        self.Bind(wx.EVT_BUTTON,self.on_click,id=10,id2=20)
+        self.Bind(wx.EVT_BUTTON,self.on_click,b2)
+        
+        #statictext = wx.StaticText(parent=panel,label="hello,world",pos=(100,100))
+    def on_click(self,event):
+        event_id = event.GetId()
+        if event_id == 11:
+            self.statictext.SetLabelText("11")
+        elif event_id == 12:
+            self.statictext.SetLabelText("12")
+        else:
+            self.statictext.SetLabelText("13")
 class App(wx.App):
+    
     def OnInit(self):
         frame = MyFrame()
         frame.Show()
